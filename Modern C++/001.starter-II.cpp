4df0a578c319ec745cc63e836f36a87268a99123
saugata-git <31741625+saugata-git@users.noreply.github.com>
2021-12-27 13:09:19 +0530

    Update 001.starter-II.cpp

--- Modern C++/001.starter-II.cpp
+++ Modern C++/001.starter-II.cpp
@@ -133,6 +133,12 @@ char* d = R"string \\"    //to define raw string.
  * 
  */
 
+
+/**
+ * @brief  lambda function
+ * 
+ */
+
 std::cout << [](int x,int y){ return x+y; }(3,4) << std::endl;     //output : 7
 auto f = [](int x,int y){ return x+y;}
 std::cout<< f(3,4) << std::endl;     //output : 7
@@ -146,6 +152,17 @@ void filter(func f, std::vector<int> arr) {
     }
 }
 
+int main(){ 
+   std::vector<int> v = { 1, 2, 3, 4, 5, 6};
+   filter([](int x) {return (x>3);}, v) ;  //output : 4, 5, 6   
+   ....
+   filter([](int x){ return (x>2 && x<5);}, v); //output : 3, 4
+
+
+   int y = 4;
+   filter([&](int x) { return (x>y);} ,v);   //output : 5, 6 
+   //Note: [&] tells compiler that we want variable capture 
+}
 
 
 
