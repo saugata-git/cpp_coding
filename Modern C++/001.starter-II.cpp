76d8334ff8c3d1ad00f37fffb62c08e3cd1d85be
saugata-git <31741625+saugata-git@users.noreply.github.com>
2021-12-27 11:44:42 +0530

    Update 001.starter-II.cpp

--- Modern C++/001.starter-II.cpp
+++ Modern C++/001.starter-II.cpp
@@ -32,6 +32,38 @@ class yellowdog : public dog {
     virtual void B() override;       //Error: no function to override 
     void C() override;               //Error: no function to override
 }
-
 //show you error message 
+
+
+
+/**
+ * @brief final( for virtual function and for class)
+ */
+
+class dog final {     //no class can be derived from dog 
+    ...
+};
+
+class dog {
+    virtual void bark() final;   //No class can override bark()
+}
+
+
+/**
+ * @brief Compiler Generated Default Constructor
+ */
+
+class dog{
+    dog(int age){}
+};
+
+dog d1; //Error: compiler will not generate the default constructor
+
+//C++ 11:
+class dog {
+    dog(int age);
+    dog() = default;   //Force compiler to generate the default constructor
+}
+
+
 
