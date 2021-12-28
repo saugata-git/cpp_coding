fef48f9a71e9b41ca8cab6e72942636a50a6fc57
saugata-git <31741625+saugata-git@users.noreply.github.com>
2021-12-28 12:11:12 +0530

    Update 002.Rvalue Reference Move Semantics.cpp

--- Modern C++/002.Rvalue Reference Move Semantics.cpp
+++ Modern C++/002.Rvalue Reference Move Semantics.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include<iterator>
+#include <vector>
 
+// C++ 11 : Rvalue Reference
+// 1. Moving Semantics
+// 2. Perfect Forwarding
+// Prerequisite: undersatnding of rvalue and lvalue
+
+
+/**
+ * @brief  Moving Semantics
+ * 
+ */
+
+void printInt(int& i){  std::cout<< "lvalue reference : "<< i <<std::endl; }
+void printInt(int&& i){  std::cout<< "rvalue reference : "<< i <<std::endl; }
+
+//void printInt(int i){ } // function overloading of this kind will not work
+
+int main(){
+
+    //what is rvalue reference?
+     
+    int a = 5;    //a is a lvalue
+   /*  int& b = a;   //b is lvalue reference (reference only in C++03)
+       int&& c       //c is rvalue reference 
+   */
+    
+    printInt(a);  // call printInt(int& i)
+    printInt(6);  // call printInt(int&& i)
+
+}
