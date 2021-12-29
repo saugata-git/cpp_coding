6d9f77569566b748277d554c047276e3c550bc57
saugata-git <31741625+saugata-git@users.noreply.github.com>
2021-12-29 11:32:04 +0530

    Update 005.User Defined Literals.cpp

--- Modern C++/005.User Defined Literals.cpp
+++ Modern C++/005.User Defined Literals.cpp
@@ -23,3 +23,27 @@ long double height = 3.4;  // Metres ? Centimeters ? Inches ?
 //Ideally, always attach units to the values:
 // height = 3.4cm;
 // ratio = 3.4cm /2.1 mm;   // ratio = 3.4 * 10 /2.1
+
+
+
+/* ------------------------------------------------------------------------------------------- */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief User defined Literals
+ */
+
+//User defined literals
+long double operator"" _cm(long double x) { return x*10; }
+long double operator"" _m(long double x) { return x*1000; }
+long double operator"" _mm(long double x) { return x; }
+
+int main(){
+   long double height = 3.4_cm;
+   cout<< height << endl;
+}
+
+
+
