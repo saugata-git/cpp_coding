9670cbe48fa8ef33e36ab080f70a16074f6563d9
saugata-git <31741625+saugata-git@users.noreply.github.com>
2021-12-29 11:20:51 +0530

    Create 005.User Defined Literals.cpp

---
+++ Modern C++/005.User Defined Literals.cpp
@@ -0,0 +1,25 @@
+/**
+ * @brief User defined Literals
+ */
+
+/**
+ * Literals are constansts.
+ * 
+ * C++ has 4 kinds of literals :
+ *    integer         ------  45
+ *    floating point  ------  4.5
+ *    character       ------  'z'
+ *    string          ------  "Dog" 
+ */
+
+// 45;  int literal
+// 45u; unsigned int (suffix specifies type)
+// 45l  long 
+
+//C++ 99 :
+long double height = 3.4;  // Metres ? Centimeters ? Inches ?
+
+
+//Ideally, always attach units to the values:
+// height = 3.4cm;
+// ratio = 3.4cm /2.1 mm;   // ratio = 3.4 * 10 /2.1
