5a3c9d7695741837be108a6b0f4625651b389da0
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-01-03 15:29:53 +0530

    Update 006.Compiler Generated Functions.cpp

--- Modern C++/006.Compiler Generated Functions.cpp
+++ Modern C++/006.Compiler Generated Functions.cpp
@@ -6,19 +6,18 @@ class Dog {} ; //not really empty some special finctions are there
 
 /**
  * C++ 03:
- *  1. default constructor (generated only when no constructor is defined by user)
- *  2. copy constructor
- *  3. copy assignment operator
+1. default constructor (generated only when no constructor is defined by user)
+ *  2. copy constructor ( generated only if 5,6 not decleared by user)
+ *  3. copy assignment operator ( generated only if 5,6 not decleared by user)
  *  4. destructor
- 
  */
 
 
 /**
  *  C++ 11:
  *  1. default constructor (generated only when no constructor is defined by user)
- *  2. copy constructor ( generated only if 5,6 not decleared by user)
- *  3. copy assignment operator ( generated only if 5,6 not decleared by user)
+ *  2. copy constructor ( generated only if 3,4,5,6 not decleared by user)
+ *  3. copy assignment operator ( generated only if2,4,5,6 not decleared by user)
  *  4. destructor
  *  5. move constructor   (genereated only if 2,3,4,6 not decleard by user)
  *  6. move assignment operator (genereated only if 2,3,4,5 not decleard by user)
