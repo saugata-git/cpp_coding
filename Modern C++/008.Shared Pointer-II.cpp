4f5e1f81fb75d2ce8303c4054c9e7de1a73653bd
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-01-17 02:20:45 +0530

    Update 008.Shared Pointer-II.cpp

--- Modern C++/008.Shared Pointer-II.cpp
+++ Modern C++/008.Shared Pointer-II.cpp
@@ -39,5 +39,66 @@ int main(){
 /*********************************************************************************/
 
 
+#include <iostream>
+#include <string>
+#include <memory>  //smart pointer
+using namespace std;
+
+class Dog{
+   string name_;
+   public:
+      Dog(string name) {
+         cout << "Dog is created: " << name << endl;
+         name_ = name;
+      }
+      Dog() {
+         cout <<"Nameless dog created." <<endl;
+         name_ ="nameless";
+      }
+      ~Dog() { 
+         cout << "dog is destroyed: " << name_ <<endl;
+      }
+      void bark() {
+         cout << "Dog " << name_ <<" rules!" << endl;
+      }
+};
+
+void foo(){
+   shared_ptr<Dog> p1 = make_shared<Dog>("Tommy"); //using default deleter : operator delete
+   shared_ptr<Dog> P2 = shared_ptr<Dog>(new Dog("Tom"),  //custom deleter
+      [](Dog* p){
+         cout << "Custom deleting. "; 
+         delete p;
+      }
+   );
+
+   shared_ptr<Dog> p3(new Dog[3]); //dog[1] and dog[2] have memory leaks.
+   shared_ptr<Dog> p4(new Dog[3],
+      [](Dog *p){
+         delete[] p;    // All 3 dogs will be deleted when p4 goes out of scope
+      }
+   );
+} 
+
+
+/********************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
