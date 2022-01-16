3874df94a7e2f148d90f2afb303088bf820459dd
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-01-16 20:18:45 +0530

    Update 007.Shared Pointer-I.cpp

--- Modern C++/007.Shared Pointer-I.cpp
+++ Modern C++/007.Shared Pointer-I.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <memory>  //smart pointer
+using namespace std;
 
+class Dog{
+   string name_;
+   public:
+      Dog(string name) {
+         cout << "Dog is created: " << name << endl;
+         name_ = name;
+      }
+      Dog() {
+         cout <<"Nameless dog created." <<endl;
+         name_ ="nameless";
+      }
+      ~Dog() { 
+         cout << "dog is destroyed: " << name_ <<endl;
+      }
+      void bark() {
+         cout << "Dog " << name_ <<"rules!" << endl;
+      }
+};
+
+void foo(){
+  Dog* p = new Dog("Tom");
+  //......
+  delete p;   //if commented memory leak
+  //...
+  p->bark();   // p is dangling pointer  - undefined behavior
+}
+
+// Teadious work -- resolution smart pointers
