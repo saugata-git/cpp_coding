0366e3f82680b068155a49148364f1b271c000a8
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-03-09 23:59:56 +0530

    Update 010. Resource_Managing_Class.cpp

--- Modern C++/010. Resource_Managing_Class.cpp
+++ Modern C++/010. Resource_Managing_Class.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <string>
+#include <memory>  
+#include <vector>
+
+// Resource Managing Class 
+
+class Person{
+public:
+   Person(std::string name) : pName(new std::string(name)) { } 
+   ~Person() {delete pName; }
+   void printName() { std::cout << *pName; }
+private:
+  std::string* pName;
+};
+
+
+int main() {
+
+   std::vector<Person> persons;
+   persons.push_back(Person("Aloe"));  //object goes out of scope
+   persons.front().printName(); //this will give us error pname is deleted
+   
+   std::cout << "Goodbye " <<std::endl;
+
+}
+
+/**
+ * C++ 03 solution:
+ * 1. define copy constuctor and copy assignment operator
+ * 2. delete copy constuctor and copy assignment operator
+ */
+
+
+/******************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
