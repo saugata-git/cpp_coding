ed9777ac4016766d03ad2fa56e9151c869610ed6
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-03-12 13:41:56 +0530

    Update 001.Const and Functions.cpp

--- Advanced C++/001.Const and Functions.cpp
+++ Advanced C++/001.Const and Functions.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+// const used with functions
+class Dog {
+    int age;
+    std::string name;
+public:
+    Dog() { 
+       age =3; 
+       name ="dummy";
+    }  
+    /*
+    void setAge(int& a){
+        age = a;
+        a++;  // will chnage value of a or passed param
+    } */
+
+    void setAge(const int& a){   //const reference
+        age = a;
+        //a++;  // will not work and will not change value of a or passed param
+    } 
+};
+int main() {
+    Dog d;
+    int i = 9;
+    d.setAge(i);
+    std::cout<< i << std::endl;
+    return 0;
+}
