38c4528ff35a2b9698be9f7b9e2d426dee479211
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 19:10:44 +0530

    Update and rename 007.Exceptions in Destructors.cpp to 006.Exceptions in Destructors.cpp

---
+++ Advanced C++/006.Exceptions in Destructors.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*------------------------------------------------*/
+/*
+* Prevent Exceptions from Leaving Destructors .
+*/
+class dog {
+    public:
+     string m_name;
+     dog(string  name) {
+        m_name = name;
+        cout<< name << " is born." <<endl;
+     }
+     ~dog() {
+        cout<< m_name << "is destroyed.\n" <<endl;
+     }
+
+     void bark() {
+        //....
+     }
+     // ...
+};
+
+
+int main() {
+
+  try {
+      dog dog1("Henry");
+      dog dog2("Bob");
+      throw 20;
+      dog1.bark();
+      dog2.bark();
+
+  } catch (int e) {
+      cout << e << " is caught" <<endl;
+  }
+   
+  return 0; 
+}
+
+/*
+OUTPUT :
+Henry is born.
+Bob is born.
+Bob is destroyed.
+Henry is destroyed.
+20 is caught.
+*/
+
+/*------------------------------------------------*/
+
+
+
+
+
+
