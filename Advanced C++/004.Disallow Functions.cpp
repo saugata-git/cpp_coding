5d709fbc5c955bc893033f7ad0f3365379f84081
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 16:35:50 +0530

    Update 004.Disallow Functions.cpp

--- Advanced C++/004.Disallow Functions.cpp
+++ Advanced C++/004.Disallow Functions.cpp
@@ -1,3 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*------------------------------------------------*/
+class OpenFile {
+public:
+    OpenFile(string filename) {
+        cout << "Open a file  :" << filename << endl;
+    }
+    //OpenFile(OpenFile& rhs) = delete;  // deleted compiler // code still will not compile any how
+    member_function(OpenFile& f) {
+            OpenFile f2(f);              // code still will not compile any how as 
+                                         // private constructors body is not defined
+    }
+    private:  
+      OpenFile(OpenFile& rhs);   // private copy constructor  // code still will not compile any how
+      OpenFile& operator = (const OpenFile& rhs);
+      //~OpenFile(); //no nobody can use open private destructor
+      ~OpenFile() {
+           cout << " OpenFile destructed "  << endl;
+      }
+};
+
+int main() {
+   
+    OpenFile f(string("FILE_NAME"));    
+    OpenFile f2(f);    // 2 objects trying to access same file
+ 
+}
+
+/*----------------------------------------------------------------*/
 class OpenFile {
 public:
     OpenFile(string filename){
