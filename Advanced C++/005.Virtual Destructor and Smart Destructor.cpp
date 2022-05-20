30dfc5f554c0fbf75f11b26ecbc31a9b763b9c8a
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 17:22:51 +0530

    Update 005.Virtual Destructor and Smart Destructor.cpp

--- Advanced C++/005.Virtual Destructor and Smart Destructor.cpp
+++ Advanced C++/005.Virtual Destructor and Smart Destructor.cpp
@@ -30,3 +30,44 @@ int main() {
     delete pd;
     return 0;
 };
+
+
+/*------------------------------------------------*/
+// we can avoid this using  virtual keyword if we use hared pointer
+
+class Dog {
+public :
+    
+    ~Dog() {        // we can avoid this using  virtual keyword if we use hared pointer
+        cout << "Dog destroyed " << endl;
+    }  
+};
+
+class Yellowdog : public Dog {
+public:
+   ~Yellowdog() {
+       cout << "Yellow dog destroyed." <<endl;
+    }
+};
+
+class DogFactory {
+public:
+   static shared_ptr<Dog> createYellowDog() {
+        return  shared_ptr<Yellowdog>(new Yellowdog());
+   }
+   // ... create other dogs
+};
+
+
+int main() {
+    shared_ptr<Dog> pd = DogFactory::createYellowDog();
+    // ... Do something with pd
+    //delete pd;
+    return 0;
+};
+
+/* Note :
+   unique_ptr only invoke Dog's destructor
+   shared_ptr will invoke both destructor
+   All classes in STL have no virtual destructor, so be careful inheriting from them.
+*/
