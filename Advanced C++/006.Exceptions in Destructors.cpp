61ce2dbee2fc6d8aeed4f5559e1f33cdaa5e28d7
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 19:13:01 +0530

    Update 006.Exceptions in Destructors.cpp

--- Advanced C++/006.Exceptions in Destructors.cpp
+++ Advanced C++/006.Exceptions in Destructors.cpp
@@ -51,8 +51,45 @@ Henry is destroyed.
 
 /*------------------------------------------------*/
 
+/*------------------------------------------------*/
+
+/*
+* Prevent Exceptions from Leaving Destructors .
+*/
+class dog {
+    public:
+     string m_name;
+     dog(string  name) {
+        m_name = name;
+        cout<< name << " is born." <<endl;
+     }
+     ~dog() {
+        cout<< m_name << "is destroyed.\n" <<endl;
+        throw 20;
+     }
+
+     void bark() {
+        //....
+     }
+     // ...
+};
 
 
+int main() {
 
+  try {
+      dog dog1("Henry");
+      dog dog2("Bob");
+      dog1.bark();
+      dog2.bark();
+
+  } catch (int e) {
+      cout << e << " is caught" <<endl;
+  }
+   
+  return 0; 
+}
+
+??? This program will crash badly.... as two exceptions are pending at same time ...
 
 
