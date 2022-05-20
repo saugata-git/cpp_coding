3ffa05bc82cf7f282dcec639ea805fca3f27e0d0
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 16:19:39 +0530

    Update 004.Disallow Functions.cpp

--- Advanced C++/004.Disallow Functions.cpp
+++ Advanced C++/004.Disallow Functions.cpp
@@ -1,1 +1,28 @@
+class OpenFile {
+public:
+    OpenFile(string filename){
+        cout << "Open a file  :" << filename << endl;
+    }
+    void destroyMe(){
+        delete this;
+    }
+private:                 //private destructor
+    ~OpenFile () {
+        cout << "OpenFile destructed ." << endl;
+    }
+};
 
+int main() {
+   
+    /*
+      This will not work  
+      OpenFile f(string("FILE_NAME")); 
+
+      This will not work  
+      OpenFile f(string("FILE_NAME"));  //on stack
+      f.destroyMe();
+    */
+    OpenFile* f = new OpenFile(string("FILE_NAME"));  //  on heap
+    f->destroyMe();
+    
+}
