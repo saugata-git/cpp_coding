b70b271551f6433e0d2a59817a6e6ac4bef7ec06
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 20:49:24 +0530

    Update 008.Assignment to Self in Assignment Operator.cpp

--- Advanced C++/008.Assignment to Self in Assignment Operator.cpp
+++ Advanced C++/008.Assignment to Self in Assignment Operator.cpp
@@ -1,1 +1,28 @@
 
+/*------------------------------------------------*/
+/*
+* Handle self-assignmnet in operator=
+*
+* Operator overload: exploite people's intuiton ans reduce their learning curve
+*/
+
+
+
+dog dd;
+dd = dd; // Looks Silly 
+
+dogs[i] = dogs[j]; // Looks lees Silly
+
+
+/* Implemeting Assignment Operator */
+class collar;
+class dog {
+    collar* pCollar;
+    dog& operator=(const dog& rhs) {
+        delete pCollar;    //  but self assignment like  dd = dd is risky as pCollar gets deleted 
+        pCollar = new collar(*rhs.pCollar);
+        return *this;
+    }
+};
+
+
