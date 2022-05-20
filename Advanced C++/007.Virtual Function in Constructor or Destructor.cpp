635012cb374b43480cb1345fa4e3c0cdc86b3825
saugata-git <31741625+saugata-git@users.noreply.github.com>
2022-05-20 20:12:29 +0530

    Update 007.Virtual Function in Constructor or Destructor.cpp

--- Advanced C++/007.Virtual Function in Constructor or Destructor.cpp
+++ Advanced C++/007.Virtual Function in Constructor or Destructor.cpp
@@ -1,1 +1,42 @@
+/*------------------------------------------------*/
+// Calling Virtual Functions in Constructor or Destructor 
 
+class Dog {
+public:
+    Dog() { 
+        cout << "Dog born." << endl; 
+    }
+    void bark() { 
+        cout << "I am a just a dog." <<endl;
+    }
+    void seeCat() {
+        bark();
+    }
+};
+
+class Yellowdog : public Dog {
+public:
+  Yellowdog() {
+      cout << "Yellow dog born." <<endl;
+  }
+  void bark() {
+      cout << "I am a yellow dog" <<endl;
+  }
+};
+
+
+int main() {
+   Yellowdog d;
+   d.seeCat();
+  return 0; 
+}
+
+/*
+Dog born.
+Yellow dog born.
+I am a just a dog.
+*/
+
+
+
+/*--------------------------------------------------------------*/
